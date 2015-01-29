44e3d0217c99879a19d326c4d3630d773ee7a901
pamela Adams <pwadams@me.com>
2015-01-29 16:04:00 -0700

    add quotes"

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
  def index
- end
+
+@quotes={
+  '— Cayla Hayes': '"gCamp has changed my life! It\'s the best tool I\'ve ever used."',
+  '— Leta Jaskolski': '"Before gCamp I was a disorderly slob. Now I\'m more organized than I\'ve ever been."',
+  '— Lavern Upton': '"Don\'t hesitate - sign up right now! You\'ll never be the same."'
+  }
+end
 end
