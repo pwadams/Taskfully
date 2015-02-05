334beccbc05a6ec22f53fcdf23c349dfd92d8c65
pamela Adams <pwadams@me.com>
2015-02-05 16:26:19 -0700

    fixed column and row order

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
    root 'welcome#index'
    get '/terms', to: 'terms#index'
    get '/about', to: 'about#index'
-   get '/faq', to: 'faq#index'
+   get '/faq', to: 'common_questions#index'
 
 
   # Example of regular route:
