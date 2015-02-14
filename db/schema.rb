fbeb8ab4ef51e8294d3a596e1ace4fb0be45e516
pamela Adams <pwadams@me.com>
2015-02-14 16:20:18 -0700

    migate users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212223606) do
+ActiveRecord::Schema.define(version: 20150213232953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20150212223606) do
     t.text    "description"
     t.boolean "is_complete", default: false
     t.date    "due_date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
