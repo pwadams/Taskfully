49d5fe4f9ef5a30df63c45d1ae89a5c77258df45
pamela Adams <pwadams@me.com>
2015-02-24 19:09:45 -0700

    added validations to user name and email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,13 @@ class UsersController < ApplicationController
 
   def create
       @user = User.new(user_params)
-      @user.save
+      if @user.save
       flash[:notice] = "User was successfully created"
       redirect_to users_path
+    else
+      render :new
   end
+end
 
   def edit
     @user = User.find(params[:id])
