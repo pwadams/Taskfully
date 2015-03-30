87006d8b5030c8620c60b81185c326d998ba76a7
pamela Adams <pwadams@me.com>
2015-03-30 10:55:56 -0600

    changed tracker api controller

--- app/controllers/tracker_projects_controller.rb
+++ app/controllers/tracker_projects_controller.rb
@@ -1,7 +1,7 @@
-def show
+class TrackerProjectsController < ApplicationController
+  def show
   tracker_api = TrackerAPI.new
   @tracker_stories ||= tracker_api.stories(current_user.pivotal_token, params[:id]) if current_user.pivotal_token
   @project_name = tracker_api.project(current_user.pivotal_token,params[:id])[:name]
-end
-
+  end
 end
