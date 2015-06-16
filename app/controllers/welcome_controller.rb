af5a625e3732d8c8a8cc77d0742f8bfbc38efaf3
pamela Adams <pwadams@me.com>
2015-06-16 13:11:23 -0600

    customized branding

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,8 @@ class WelcomeController < PublicController
  def index
 
 @quotes={
-  '— Cayla Hayes' => '"gCamp has changed my life! It\'s the best tool I\'ve ever used."',
-  '— Leta Jaskolski' => '"Before gCamp I was a disorderly slob. Now I\'m more organized than I\'ve ever been."',
+  '— Cayla Hayes' => '"TaskFull has changed my life! It\'s the best tool I\'ve ever used."',
+  '— Leta Jaskolski' => '"Before TaskFull I was disorderly. Now I\'m more organized than I\'ve ever been."',
   '— Lavern Upton' => '"Don\'t hesitate - sign up right now! You\'ll never be the same."'
   }
 end
